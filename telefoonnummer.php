09986f9a18c1500bf7c732984fbf4f4d0f2d1e0d
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-06-12 11:54:21 +0200

    af

--- telefoonnummer.php
+++ telefoonnummer.php
@@ -1,9 +1,9 @@
 <?php
-$pattern = '/[0-9]{10}/m';
-
+$pattern = '/[+][0-9]{11}/m';
+$pattern2 = '/[0-9]{10}/m';
 $input = readline("telnummer pls");
 
-if (preg_match($pattern, $input))
+if (preg_match($pattern, $input) || preg_match($pattern2 , $input))
 {
     echo("$input is een geldig nummer");
 }
