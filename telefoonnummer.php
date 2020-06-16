20645ebf023da0a23cb49936a393cbc991959ccf
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-06-16 14:45:18 +0200

    Uitwerking van de exercise

--- telefoonnummer.php
+++ telefoonnummer.php
@@ -1,9 +1,10 @@
 <?php
 $pattern = '/[+][0-9]{11}/m';
 $pattern2 = '/0[0-9]{9}/m';
+$pattern3 =  '/[+]316[0-9]{8}/m';
 $input = $argv[1];
 
-	if (preg_match($pattern, $input) || preg_match($pattern2 , $input))
+	if (preg_match($pattern, $input) || preg_match($pattern2 , $input) || preg_match($pattern3 , $input))
 	{
 	    echo("$input is een geldig telefoonnummer");
 	}
