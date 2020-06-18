d475196d91171b339767a6faae573ee695b4fd9b
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-06-18 10:56:53 +0200

    blast doet irritant

--- telefoonnummer.php
+++ telefoonnummer.php
@@ -1,13 +1,13 @@
 <?php
+
 $pattern = '/[+][0-9]{11}/m';
 $pattern2 = '/0[0-9]{9}/m';
-$pattern3 =  '/[+]316[0-9]{8}/m';
+$pattern3 = '/[+]316[0-9]{8}/m';
 $input = $argv[1];
-if (preg_match($pattern, $input) || preg_match($pattern2 , $input) || preg_match($pattern3 , $input)){
- 	    echo("$input is een geldig telefoonnummer");
-	}
-	else{
-	    echo("$input is niet een geldig telefoonnummer");
-	}
+if (preg_match($pattern, $input) || preg_match($pattern2, $input) || preg_match($pattern3, $input)) {
+    echo("$input is een geldig telefoonnummer");
+} else {
+    echo("$input is niet een geldig telefoonnummer");
+}
 
 
